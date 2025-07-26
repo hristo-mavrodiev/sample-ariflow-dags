e1a27a7fd68fe6ffb7c8dc41c7dec3aa37237ab9
Hristo M <h.mavrodiev@abv.bg>
2025-07-26 09:57:49 +0300

    fix dag import

--- ibex_scraper.py
+++ ibex_scraper.py
@@ -1,6 +1,6 @@
 
 from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from airflow.utils.dates import days_ago
 from datetime import timedelta
 
